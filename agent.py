b62837e89cbf63007f586813da0fc26b36f56be5
Cynthia Chen <RPC2@users.noreply.github.com>
2019-12-09 20:56:45 +0800

    organize code

--- agent.py
+++ agent.py
@@ -32,9 +32,6 @@ class Agent(AgentConfig, EnvConfig):
         self.memory = {
             'state': [], 'action': [], 'reward': [], 'next_state': [], 'action_prob': [], 'terminal': []
         }
-        f = open("log.txt", "w")
-        f.write(str(datetime.datetime.now()) + "\n")
-        f.close()
 
     def new_random_game(self):
         self.env.reset()
@@ -64,7 +61,6 @@ class Agent(AgentConfig, EnvConfig):
             # Get initial state
             state, reward, action, terminal = self.new_random_game()
             current_state = state
-            # current_state = np.stack((state, state, state, state))
 
             # A step in an episode
             while not solved:
@@ -102,8 +98,6 @@ class Agent(AgentConfig, EnvConfig):
                     print('episode: %.2f, total step: %.2f, last_episode length: %.2f, last_episode_reward: %.2f, '
                           'loss: %.4f' % (episode, step, episode_length, total_episode_reward, self.loss))
 
-                    # print(prob_a)
-
                     self.env.reset()
 
                     if self.gif:
@@ -112,10 +106,6 @@ class Agent(AgentConfig, EnvConfig):
                     break
 
                 if step % self.horizon == 0:
-                    f = open("log.txt", "a+", buffering=1)
-                    f.write(
-                        "===================================== update %d ====================================\n" % (step // self.horizon))
-
                     for _ in range(self.k_epoch):
                         self.update_network()
                     self.empty_memory()
@@ -131,66 +121,37 @@ class Agent(AgentConfig, EnvConfig):
         self.env.close()
 
     def update_network(self):
-        # get delta
-        # print("current state", self.memory['state'])
-        # print("next state", self.memory['next_state'])
-        f = open("log.txt", "a+")
         td_target = torch.FloatTensor(self.memory['reward']).to(device) + \
                     self.gamma * self.policy_network.v(torch.FloatTensor(self.memory['next_state'])) * \
                     torch.FloatTensor(self.memory['terminal'])
         delta = td_target - self.policy_network.v(torch.FloatTensor(self.memory['state']).to(device))
-        # print(delta)
         delta = delta.detach().numpy()
-        f.write('delta: ' + str(delta) + "\n")
 
         # get advantage
         advantages = []
         adv = 0.0
-        # print('delta[::-1]', delta[::-1])
         for d in delta[::-1]:
             # print('d[0]', d[0])
             adv = self.gamma * self.lmbda * adv + d[0]
-            f.write("adv: " + str(adv) + '\n')
             advantages.append([adv])
         advantages.reverse()
         advantages = torch.FloatTensor(advantages)
-        f.write('advantages: ' + str(advantages) + '\n')
 
         # get ratio
         pi = self.policy_network.pi(torch.FloatTensor(self.memory['state']).to(device))
-        f.write('pi: ' + str(pi) + '\n')
-        f.write('actions from memory: ' + str(self.memory['action']) + '\n')
-        f.write('action_prob: ' + str(self.memory['action_prob']) + '\n')
         new_probs_a = torch.gather(pi, 1, torch.tensor(self.memory['action']))
         old_pi = self.old_policy.pi(torch.FloatTensor(self.memory['state']).to(device))
         old_probs_a = torch.gather(old_pi, 1, torch.tensor(self.memory['action']))
-        f.write('new_probs_a: ' + str(new_probs_a) + '\n')
-        f.write('old_probs_a: ' + str(old_probs_a) + '\n')
-        f.write("log new probs: " + str(torch.log(new_probs_a)) + '\n')
-        f.write("log old probs: " + str(torch.log(old_probs_a)) + '\n')
-        # ratio = torch.exp(torch.log(new_probs_a) - torch.log(torch.tensor(self.memory['action_prob'])))
         ratio = torch.exp(torch.log(new_probs_a) - torch.log(old_probs_a))
-        f.write('ratio: ' + str(ratio) + '\n')
 
         # surrogate loss
         surr1 = ratio * advantages
         surr2 = torch.clamp(ratio, 1 - self.eps_clip, 1 + self.eps_clip) * advantages
         pred_v = self.policy_network.v(torch.FloatTensor(self.memory['state']).to(device))
-        f.write('surr1: ' + str(surr1) + '\n')
-        f.write('surr2: ' + str(surr2) + '\n')
-        f.write('min of surr1 and surr2: ' + str(-torch.min(surr1, surr2)) + '\n')
-        f.write('pred_v: ' + str(pred_v) + '\n')
-        f.write('td_target: ' + str(td_target) + '\n')
         v_loss = 0.5 * (pred_v - td_target).pow(2)  # Huber loss
-        f.write('v_loss: ' + str(v_loss) + '\n')
         entropy = torch.distributions.Categorical(pi).entropy()
         entropy = torch.tensor([[e] for e in entropy])
-        f.write('entropy: ' + str(entropy) + '\n')
         self.loss = (-torch.min(surr1, surr2) + self.v_coef * v_loss - self.entropy_coef * entropy).mean()
-        f.write('min of surr1, surr2: ' + str((-torch.min(surr1, surr2)).mean()) + '\n')
-        f.write('v loss: ' + str((self.v_coef * v_loss).mean()) + '\n')
-        f.write('entropy loss: ' + str((- self.entropy_coef * entropy).mean()) + '\n')
-        f.write('loss: ' + str(self.loss) + '\n')
 
         self.old_policy.load_state_dict(self.policy_network.state_dict())
 
